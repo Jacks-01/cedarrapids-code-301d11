38df8f4c596fa8c6f7cd826aee8c52d122f0a1f3
Bob Trapp <bobgtrapp@gmail.com>
2022-05-20 14:46:00 -0500

    Added Class 10 demo

---
+++ class-10/demo/front-end/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          Recipes from{" "}
+          <a href="https://www.edamam.com/" target="_blank">
+            Edamam
+          </a>
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
