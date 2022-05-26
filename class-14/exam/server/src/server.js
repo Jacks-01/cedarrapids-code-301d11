f022be0b05d6c55cfadd6cf15bf30955fa116145
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-26 14:14:39 -0500

    missed a typo

--- class-14/exam/server/src/server.js
+++ class-14/exam/server/src/server.js
@@ -19,7 +19,7 @@ app.use('*', (req,res) => {
   res.status(404).send('These are not the droids you are looking for.');
 });
 
-app.use( (error,req,res,next) => {
+app.use( (error,req,res) => {
   res.status(500).send(`My Bad ... ${error.message}`);
 });
 
